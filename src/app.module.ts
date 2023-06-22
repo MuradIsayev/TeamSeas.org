724e73775a37e487fc3f05a517968fa02a75df70
MurAd <muradisayevazer@gmail.com>
2023-06-22 21:57:07 +0400

    donations crud created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+import { DonationsModule } from './donations/donations.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-co
       ],
       debug: true,
     }),
+    DonationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
