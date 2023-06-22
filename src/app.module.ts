ddd70c660db2284bec24909e7181de38a52f286a
MurAd <muradisayevazer@gmail.com>
2023-06-22 21:54:16 +0400

    graphql module imported with pre-settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      playground: false,
+      plugins: [
+         ApolloServerPluginLandingPageGraphQLPlayground(),
+      ],
+      debug: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
